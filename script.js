5badf447b7130e047e46b9a7934fb428527acdc8
Finncrisp8 <90760704+Finncrisp8@users.noreply.github.com>
2024-04-29 14:09:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-
     var map;
-    require(["esri/map", "dojo/domReady!"], function(Map) {
+    require(["esri/map", 
+    "esri/map",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    "dojo/domReady!"], function(Map, ArcGISDynamicMapServiceLayer) {
         map = new Map("mapDiv", {
             basemap: "topo",
             center: [17.923597762, 60.208251508],
             zoom: 10
         });
+
+        var CanoeLayer = new ArcGISDynamicMapServiceLayer();
+
+        var BikeLayer = new ArcGISDynamicMapServiceLayer();
+
+        var WalkLayer = new ArcGISDynamicMapServiceLayer();
+
+        map.addLayer(CanoeLayer);
+        map.addLayer(BikeLayer);
+        map.addLayer(WalkLayer);
+
+        CanoeLayer.setOpacity(1);
+        BikeLayer.setOpacity(1);
+        WalkLayer.setOpacity(1);
+
     });
