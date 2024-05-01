00e28c054b7d1cceccbd33bf6c81e02cf86f443f
Finncrisp8 <90760704+Finncrisp8@users.noreply.github.com>
2024-05-01 20:24:19 +0200

    Update script.js 1

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/GraphicsLayer",
+    "esri/Graphic",
+    "esri/geometry/Polyline",
+    "esri/symbols/SimpleLineSymbol"
+], function(Map, MapView, GraphicsLayer, Graphic, Polyline, SimpleLineSymbol) {
+    const map = new Map({
+        basemap: "topo-vector"
+    });
 
-    require([
-        "esri/map",
-        "esri/symbols/SimpleMarkerSymbol",
-        "esri/graphic",
-        "esri/layers/GraphicsLayer",
-        "esri/InfoTemplate",
-        "esri/geometry/Point",
-        "dojo/_base/Color",
-        "dojo/domReady!"
-      ], function(Map, SimpleMarkerSymbol, Graphic, GraphicsLayer, InfoTemplate, Point, Color) {
-        var map = new Map("mapDiv", {
-          basemap: "topo",
-          center: [17.923597762, 60.208251508],
-          zoom: 10
+    const view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [17.923597762, 60.208251508], // Longitude, latitude
+        zoom: 10
+    });
+
+    // Function to add trail graphics to the map
+    function addTrailGraphics(trailCoordinates, color) {
+        const graphicsLayer = new GraphicsLayer();
+        map.add(graphicsLayer);
+
+        const coordinates = trailCoordinates.map(coord => [parseFloat(coord.longitude), parseFloat(coord.latitude)]);
+
+        const polyline = new Polyline({
+            paths: coordinates
         });
-      
-        var graphicsLayer = new GraphicsLayer();
-        map.addLayer(graphicsLayer);
-      
-        var simpleMarkerSymbol = new SimpleMarkerSymbol();
-        simpleMarkerSymbol.setStyle(SimpleMarkerSymbol.STYLE_CIRCLE);
-        simpleMarkerSymbol.setSize(16);
-        simpleMarkerSymbol.setColor(new Color([255, 0, 0, 0.51]));
-      
-        var graphic = new Graphic(
-          new Point(17.923597762, 60.208251508),
-          simpleMarkerSymbol
-        ).setInfoTemplate(new InfoTemplate("Test"));
-        graphicsLayer.add(graphic);
-      });
-      
+
+        const trailGraphic = new Graphic({
+            geometry: polyline,
+            symbol: new SimpleLineSymbol({
+                color: color || [0, 0, 255],
+                width: 2
+            })
+        });
+
+        graphicsLayer.add(trailGraphic);
+    }
+
+    // Function to handle errors
+    function handleError(error) {
+        console.error("Error: ", error);
+    }
+
+    // Load trail data from JSON files and add them to the map
+    fetch('coordinates.json')
+        .then(response => response.json())
+        .then(data => addTrailGraphics(data.posts, [255, 0, 0]))
+        .catch(handleError);
+
+    fetch('coord.json')
+        .then(response => response.json())
+        .then(data => addTrailGraphics(data.posts, [0, 255, 0]))
+        .catch(handleError);
+
+    fetch('coord2.json')
+        .then(response => response.json())
+        .then(data => addTrailGraphics(data.posts, [0, 0, 255]))
+        .catch(handleError);
+});
+
 
